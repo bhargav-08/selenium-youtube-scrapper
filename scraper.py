f11248e30e9a1ae786d281c751ae1bbad15c011a
BHARGAV CHANDPARA <90968369+bhargav-08@users.noreply.github.com>
2021-11-30 16:39:28 +0000

    added selenium

--- scraper.py
+++ scraper.py
@@ -1,1 +1,22 @@
-print("Hello World")
+from selenium import  webdriver
+from selenium.webdriver.chrome.options import Options
+
+def get_driver():
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+
+if __name__=="__main__":
+
+    YOUTUBE_TRENDING = "https://www.youtube.com/feed/trending"
+
+    driver = get_driver()
+    driver.get(YOUTUBE_TRENDING)
+
+    print(driver.title)
+
+
